8ba25e47096f3084839b90d4941e6ec5f3f5d986
Maia Ferrer <maiaferrer@Maias-MacBook-Air.local>
2023-01-09 20:41:14 -0500

    added pseudo code

---
+++ server.js
@@ -0,0 +1,10 @@
+// html:
+//  Get /notes  => returns notes.html file
+
+
+// create API routes by sending data in a JSON
+// GET /api/notes
+
+// POST /api/notes
+
+// END: Get (wild card entry) * => returns the index.html file
