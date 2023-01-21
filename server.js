34af711d74e4600b03b1b60e24b30d6d5d3a433b
Maia Ferrer <maiaferrer@Maias-MacBook-Air.local>
2023-01-21 10:35:02 -0500

    added modular routes, middleware for paring, and a server listening

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 // Require Dependancies
 const express = require('express');
 const app = express();
-const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 // import modular routers for /api and /
 // api route
-
+const api = require("./routes/api");
+app.use(api);
 // html route
-
-
+const html = require("./routes/html");
+app.use(html);
 // Middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 // server listener
